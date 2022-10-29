9873e43af53212df5caf9199858d289b33af4baf
Alain <yinxulai@hotmail.com>
2022-10-29 15:39:15 +0800

    add static build & actions

--- next.config.js
+++ next.config.js
@@ -60,6 +60,9 @@ const nextConfig = withNextra({
   experimental: {
     legacyBrowsers: false,
   },
+  images: {
+    unoptimized: true
+  },
   webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.DefinePlugin({
@@ -71,106 +74,106 @@ const nextConfig = withNextra({
     // return the modified config
     return config;
   },
-  rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/sitemap.xml",
-          destination:
-            "https://crawled-sitemap.vercel.sh/turbobuild-sitemap.xml",
-        },
-      ],
-    };
-  },
-  async redirects() {
-    return [
-      ...OLD_TURBOREPO_ROUTES.map((route) => ({
-        source: route,
-        destination: `/repo${route}`,
-        permanent: true,
-      })),
-      {
-        source: "/usage",
-        destination: "/reference/command-line-reference",
-        permanent: true,
-      },
-      {
-        source: "/docs/core-concepts/running-tasks",
-        destination: "/repo/docs/core-concepts/monorepos/running-tasks",
-        permanent: true,
-      },
-      {
-        source: "/docs/core-concepts/why-turborepo",
-        destination: "/repo/docs/core-concepts/monorepos",
-        permanent: true,
-      },
-      {
-        source: "/docs/core-concepts/filtering",
-        destination: "/repo/docs/core-concepts/monorepos/filtering",
-        permanent: true,
-      },
-      {
-        source: "/docs/guides/workspaces",
-        destination: "/docs/handbook/workspaces",
-        permanent: true,
-      },
-      {
-        source: "/docs/core-concepts/workspaces",
-        destination: "/docs/handbook/workspaces",
-        permanent: true,
-      },
-      {
-        source: "/docs/core-concepts/pipelines",
-        destination: "/docs/core-concepts/running-tasks",
-        permanent: true,
-      },
-      {
-        source: "/docs/guides/migrate-from-lerna",
-        destination: "/docs/handbook/migrating-to-a-monorepo",
-        permanent: true,
-      },
-      {
-        source: "/docs/getting-started",
-        destination: "/docs",
-        permanent: true,
-      },
-      {
-        source: "/discord{/}?",
-        permanent: true,
-        destination: "https://discord.gg/sSzyjxvbf5",
-      },
-      {
-        source: "/docs/changelog",
-        permanent: true,
-        destination: "https://github.com/vercel/turbo/releases",
-      },
-      {
-        source: "/docs/guides/complimentary-tools",
-        permanent: true,
-        destination: "/docs/handbook",
-      },
-      {
-        source: "/docs/guides/monorepo-tools",
-        permanent: true,
-        destination: "/docs/handbook",
-      },
-      {
-        source: "/docs/glossary",
-        permanent: true,
-        destination: "/docs/handbook",
-      },
-      {
-        source: "/docs/guides/continuous-integration",
-        permanent: true,
-        destination: "/docs/ci",
-      },
-      {
-        source: "/docs/features/:path*",
-        permanent: true,
-        destination: "/docs/core-concepts/:path*",
-      },
-    ];
-  },
+  // rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: "/sitemap.xml",
+  //         destination:
+  //           "https://crawled-sitemap.vercel.sh/turbobuild-sitemap.xml",
+  //       },
+  //     ],
+  //   };
+  // },
+  // async redirects() {
+  //   return [
+  //     ...OLD_TURBOREPO_ROUTES.map((route) => ({
+  //       source: route,
+  //       destination: `/repo${route}`,
+  //       permanent: true,
+  //     })),
+  //     {
+  //       source: "/usage",
+  //       destination: "/reference/command-line-reference",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/core-concepts/running-tasks",
+  //       destination: "/repo/docs/core-concepts/monorepos/running-tasks",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/core-concepts/why-turborepo",
+  //       destination: "/repo/docs/core-concepts/monorepos",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/core-concepts/filtering",
+  //       destination: "/repo/docs/core-concepts/monorepos/filtering",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/guides/workspaces",
+  //       destination: "/docs/handbook/workspaces",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/core-concepts/workspaces",
+  //       destination: "/docs/handbook/workspaces",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/core-concepts/pipelines",
+  //       destination: "/docs/core-concepts/running-tasks",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/guides/migrate-from-lerna",
+  //       destination: "/docs/handbook/migrating-to-a-monorepo",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/getting-started",
+  //       destination: "/docs",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/discord{/}?",
+  //       permanent: true,
+  //       destination: "https://discord.gg/sSzyjxvbf5",
+  //     },
+  //     {
+  //       source: "/docs/changelog",
+  //       permanent: true,
+  //       destination: "https://github.com/vercel/turbo/releases",
+  //     },
+  //     {
+  //       source: "/docs/guides/complimentary-tools",
+  //       permanent: true,
+  //       destination: "/docs/handbook",
+  //     },
+  //     {
+  //       source: "/docs/guides/monorepo-tools",
+  //       permanent: true,
+  //       destination: "/docs/handbook",
+  //     },
+  //     {
+  //       source: "/docs/glossary",
+  //       permanent: true,
+  //       destination: "/docs/handbook",
+  //     },
+  //     {
+  //       source: "/docs/guides/continuous-integration",
+  //       permanent: true,
+  //       destination: "/docs/ci",
+  //     },
+  //     {
+  //       source: "/docs/features/:path*",
+  //       permanent: true,
+  //       destination: "/docs/core-concepts/:path*",
+  //     },
+  //   ];
+  // },
 });
 
 // module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
