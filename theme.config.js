7f76e807fcf94e076de5d4e6b9d57681bc027b0d
Alain <yinxulai@hotmail.com>
2022-11-01 12:13:22 +0800

    update

--- theme.config.js
+++ theme.config.js
@@ -45,7 +45,7 @@ const theme = {
   },
   font: false,
   feedback: {
-    link: "Question? Give us feedback →",
+    link: "碰到问题？在这里请求帮助 ->",
   },
   logo: HeaderLogo,
   logoLink: false,
@@ -75,8 +75,20 @@ const theme = {
       ogUrl = `https://turbo-site-og.vercel.app/api/og?type=${type}${title}`;
     }
 
+    const baiduStatisticsScript = `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement(\"script\");
+        hm.src = "https://hm.baidu.com/hm.js?8e5291f90d0b584803c5c0e31939218e";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `
+
     return (
       <>
+        <meta name="keywords" content="turbo，turborepo，turbopack，turborepo中文文档，turbopack中文文档，turborepo汉化，turbopack汉化"></meta>
+        <meta name="description" content="Turbo 是一个用 Rust 实现的 JavaScript 和 TypeScript 高性能打包构建工具"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="apple-touch-icon"
@@ -120,11 +132,12 @@ const theme = {
         <link rel="prefetch" href="/repo/docs" as="document" />
         <link rel="prefetch" href="/pack" as="document" />
         <link rel="prefetch" href="/pack/docs" as="document" />
+        <script dangerouslySetInnerHTML={{ __html: baiduStatisticsScript}}></script>
       </>
     );
   },
   editLink: {
-    text: "Edit this page on GitHub",
+    text: "帮助我们修订这篇文档",
   },
   navbar: Navigation,
   footer: {
